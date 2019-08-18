fbb50f71b0b725b88a83e6c5abe72d6875c96e03
Per Malmberg <per.malmberg@gmail.com>
2019-08-18 14:29:49 +0200

    Sensor data published via Websocket.

--- software/main/http/WSDataConnection.cpp
+++ software/main/http/WSDataConnection.cpp
@@ -1,6 +1,8 @@
 #include "WSDataConnection.h"
 #include <smooth/core/json/Value.h>
 #include <smooth/application/network/http/websocket/responses/WSResponse.h>
+#include <smooth/application/network/http/IServerResponse.h>
+#include <chrono>
 
 using namespace smooth::core::json;
 using namespace smooth::application::network::http::websocket::responses;
@@ -10,33 +12,46 @@ namespace http
     void
     WSDataConnection::data_received(bool first_part, bool last_part, bool is_text, const std::vector<uint8_t>& data)
     {
-
     }
 
     void WSDataConnection::event(const AnalogValue& value)
     {
         Value v;
-        auto a = v["analog"];
+        auto a = v["input"]["analog"];
         a["name"] = value.get_name();
         a["value"] = value.get_value();
         a["input"] = value.get_input();
-        WSResponse wsr{v.to_string(), true, true};
-        //response.reply()
+        response.reply(std::make_unique<WSResponse>(v.to_string(), true, true), false);
     }
 
     void WSDataConnection::event(const DigitalValue& value)
     {
-
+        Value v;
+        auto a = v["input"]["digital"];
+        a["name"] = value.get_name();
+        a["value"] = value.get_value();
+        a["input"] = value.get_input();
+        response.reply(std::make_unique<WSResponse>(v.to_string(), true, true), false);
     }
 
     void WSDataConnection::event(const SensorValue& value)
     {
-
+        Value v;
+        auto a = v["sensor"];
+        a["humidity"] = value.get_humidity();
+        a["pressure"] = value.get_pressure();
+        a["temperature"] = value.get_temperature();
+        response.reply(std::make_unique<WSResponse>(v.to_string(), true, true), false);
     }
 
     void WSDataConnection::event(const DigitalOutputValue& value)
     {
-
+        Value v;
+        auto a = v["output"]["digital"];
+        a["name"] = value.get_name();
+        a["value"] = value.get_value();
+        a["output"] = value.get_output();
+        response.reply(std::make_unique<WSResponse>(v.to_string(), true, true), false);
     }
 
     void WSDataConnection::event(const DigitalStatusOutputValue& value)
@@ -46,11 +61,23 @@ namespace http
 
     void WSDataConnection::event(const g3::alarm::event::SensorTriggered& value)
     {
-
+        Value v;
+        auto a = v["sensor"];
+        a["triggered"] = true;
+        a["name"] = value.get_name();
+        a["entry_delay"] = static_cast<uint32_t>(std::chrono::duration_cast<std::chrono::milliseconds>(
+                value.get_entry_delay()).count());
+        a["exit_delay"] = static_cast<uint32_t>(std::chrono::duration_cast<std::chrono::milliseconds>(
+                value.get_exit_delay()).count());
+        response.reply(std::make_unique<WSResponse>(v.to_string(), true, true), false);
     }
 
     void WSDataConnection::event(const g3::alarm::event::SensorRestored& value)
     {
-
+        Value v;
+        auto a = v["sensor"];
+        a["triggered"] = false;
+        a["name"] = value.get_name();
+        response.reply(std::make_unique<WSResponse>(v.to_string(), true, true), false);
     }
 }
